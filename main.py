423c50e1602024be0ccbd25e99abae6c2bf2bb1e
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-01-11 08:02:22 +0200

    todo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ TODO:
 DONE make encoding queue with limiting by workers
 DONE make concatenating videos after encoding
 DONE make passing your arguments for encoding,
+2-pass encode by default for better quality
 make separate audio and encode it separately,
 """
 #import asyncio

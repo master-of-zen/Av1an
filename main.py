62fc634b417479c32f90a085063da8817c00963e
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-01-11 13:06:29 +0200

    adding second pass attempts

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ except ImportError:
     print('ERROR: No PyScenedetect installed, try: sudo pip install scenedetect')
 
 #-w 252 -h 144
-DEFAULT_ENCODE = ' -w 35 -h 20 --cpu-used=8 --end-usage=q --cq-level=63 --aq-mode=0'
+DEFAULT_ENCODE = ' --cpu-used=8 --end-usage=q --cq-level=63 --aq-mode=0'
 DEFAULT_AUDIO = '-c:a libopus -ac 1 -b:a 12k'
 FFMPEG = 'ffmpeg -hide_banner -loglevel warning '
 
@@ -137,10 +137,9 @@ def encode(commands):
     TODO:
     Replace ffmpeg with aomenc because ffmpeg libaom doen't work with parameters properly
     """
-
-    cmd = f'{FFMPEG} {commands[0]}'
-
-    Popen(cmd, shell=True).wait()
+    for i in commands[:-1]:
+        cmd = f'{FFMPEG} {i}'
+        Popen(cmd, shell=True).wait()
 
 
 def concatenate_video(input_video):
@@ -170,7 +169,8 @@ def compose_encoding_queue(encoding_params, files):
     """
     ffmpeg_pipe = '-pix_fmt yuv420p -f yuv4mpegpipe - |'
     single_pass = 'aomenc -q --passes=1 '
-    two_pass = ''
+    two_pass_1 = '--passes=2 --pass=1'
+    two_pass_2 = ' --pass=2'
     file_paths = [(f'{join(os.getcwd(), "temp", "split", file_name)}',
                    f'{join(os.getcwd(), "temp", "encode", file_name)}',
                    file_name) for file_name in files]
@@ -180,11 +180,14 @@ def compose_encoding_queue(encoding_params, files):
          f' {single_pass} {encoding_params} -o {file[1]} -',  file[2])
         for file in file_paths]
 
-    pass_2_commands= [
+    pass_2_commands = [
         (f'-i {file[0]} {ffmpeg_pipe}' +
-         f'')
+         f' aomenc -q {two_pass_1} {encoding_params} --fpf={file[1]}.log -o /dev/null -',
+         f' aomenc -q {two_pass_2} {encoding_params} --fpf={file[1]}.log -o {file[1]} -'
+         , file[2])
         for file in file_paths]
-
+    print(pass_1_commands[0])
+    print(pass_2_commands[0])
     return pass_1_commands
 
 

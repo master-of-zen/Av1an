2cec9794cf339d362c09a5d83da4d5ab7e67cd3d
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-03-13 14:01:17 +0200

    better doc strings

--- av1an.py
+++ av1an.py
@@ -29,9 +29,8 @@ if sys.version_info < (3, 7):
 class Av1an:
 
     def __init__(self):
-        """
-        Av1an - AV1 wrapper for AV1 encoders
-        """
+        """Av1an - AV1 wrapper for AV1 encoders"""
+
         self.temp_dir = Path('.temp')
 
         self.FFMPEG = 'ffmpeg -y -hide_banner -loglevel error'
@@ -52,10 +51,12 @@ class Av1an:
         self.skip_scenes = False
 
     def log(self, info):
+        """Default logging function, write to file"""
         with open(self.logging, 'a') as log:
             log.write(time.strftime('%X') + ' ' + info)
 
     def call_cmd(self, cmd, capture_output=False):
+        """Calling system shell, if capture_ouput=True output string will be returned"""
         if capture_output:
             return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
 
@@ -63,8 +64,7 @@ class Av1an:
             subprocess.run(cmd, shell=True, stdout=log, stderr=log)
 
     def arg_parsing(self):
-
-        # Command line parse and assigning defined and user defined params
+        """Command line parse and assigning defined and user defined params"""
 
         parser = argparse.ArgumentParser()
         parser.add_argument('--mode', '-m', type=int, default=self.mode, help='Mode 0 - video, Mode 1 - image')

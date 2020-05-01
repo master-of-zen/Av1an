1e25c555a9156cc3adbd7badeba84772124a69ef
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-05-01 21:29:20 +0300

    Changed vmaf steps

--- av1an.py
+++ av1an.py
@@ -83,7 +83,7 @@ class Av1an:
         parser.add_argument('--vmaf_path', type=str, default=None, help='Path to vmaf models')
         parser.add_argument('--tg_vmaf', type=float, help='Value of Vmaf to target')
         parser.add_argument('--vmaf_error', type=float, default=0.0, help='Error to compensate to wrong target vmaf')
-        parser.add_argument('--vmaf_steps', type=int, default=4, help='Steps between min and max qp for target vmaf')
+        parser.add_argument('--vmaf_steps', type=int, default=5, help='Steps between min and max qp for target vmaf')
         parser.add_argument('--min_cq', type=int, default=20, help='Min cq for target vmaf')
         parser.add_argument('--max_cq', type=int, default=63, help='Max cq for target vmaf')
         parser.add_argument('--host', type=str, help='ip of host')
@@ -137,8 +137,8 @@ class Av1an:
 
         self.d['ffmpeg_pipe'] = f' {self.d.get("ffmpeg")} {self.d.get("pix_format")} -f yuv4mpegpipe - |'
 
-        if self.d.get('vmaf_steps') < 4:
-            print('Target vmaf require more than 3 probes/steps')
+        if self.d.get('vmaf_steps') < 5:
+            print('Target vmaf require more than 4 probes/steps')
             sys.exit()
 
     def determine_resources(self):

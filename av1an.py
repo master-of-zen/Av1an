25ec0e1175fc002821204989bf042361cc62e8f5
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-06-17 13:22:46 +0300

    moved arg parse out

--- av1an.py
+++ av1an.py
@@ -17,6 +17,7 @@ from multiprocessing.managers import BaseManager
 from pathlib import Path
 from subprocess import PIPE, STDOUT
 from tqdm import tqdm
+from utils.arg_parse import arg_parsing
 from utils.aom_keyframes import aom_keyframes
 from utils.boost import boosting
 from utils.compose import aom_vpx_encode, rav1e_encode, svt_av1_encode, compose_encoding_queue, get_video_queue
@@ -149,72 +150,6 @@ class Av1an:
                 print(f'No such model: {Path(self.d.get("vmaf_path")).as_posix()}')
                 terminate()
 
-    def arg_parsing(self):
-        """Command line parsing"""
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--mode', '-m', type=int, default=0, help='0 - local, 1 - master, 2 - encoder')
-
-        # Input/Output/Temp
-        parser.add_argument('--input', '-i', nargs='+', type=Path, help='Input File')
-        parser.add_argument('--temp', type=Path, default=Path('.temp'), help='Set temp folder path')
-        parser.add_argument('--output_file', '-o', type=Path, default=None, help='Specify output file')
-
-        # Splitting
-        parser.add_argument('--split_method', type=str, default='pyscene', help='Specify splitting method')
-        parser.add_argument('--extra_split', '-xs', type=int, default=0, help='Number of frames after which make split')
-
-
-        # PySceneDetect split
-        parser.add_argument('--scenes', '-s', type=str, default=None, help='File location for scenes')
-        parser.add_argument('--threshold', '-tr', type=float, default=50, help='PySceneDetect Threshold')
-
-        # Encoding
-        parser.add_argument('--passes', '-p', type=int, default=2, help='Specify encoding passes')
-        parser.add_argument('--video_params', '-v', type=str, default='', help='encoding settings')
-        parser.add_argument('--encoder', '-enc', type=str, default='aom', help='Choosing encoder')
-        parser.add_argument('--workers', '-w', type=int, default=0, help='Number of workers')
-        parser.add_argument('-cfg', '--config', type=Path, help='Parameters file. Save/Read: '
-                                                                'Video, Audio, Encoder, FFmpeg parameteres')
-
-        # FFmpeg params
-        parser.add_argument('--ffmpeg', '-ff', type=str, default='', help='FFmpeg commands')
-        parser.add_argument('--audio_params', '-a', type=str, default='-c:a copy', help='FFmpeg audio settings')
-        parser.add_argument('--pix_format', '-fmt', type=str, default='yuv420p', help='FFmpeg pixel format')
-
-        # Misc
-        parser.add_argument('--logging', '-log', type=str, default=None, help='Enable logging')
-        parser.add_argument('--resume', '-r', help='Resuming previous session', action='store_true')
-        parser.add_argument('--no_check', '-n', help='Do not check encodings', action='store_true')
-        parser.add_argument('--keep', help='Keep temporally folder after encode', action='store_true')
-
-        # Boost
-        parser.add_argument('--boost', help='Experimental feature, decrease CQ of clip based on brightness.'
-                                            'Darker = lower CQ', action='store_true')
-        parser.add_argument('--boost_range', default=15, type=int, help='Range/strength of CQ change')
-        parser.add_argument('--boost_limit', default=10, type=int, help='CQ limit for boosting')
-
-        # Grain
-        # Todo: grain stuf
-        # parser.add_argument('--grain', help='Exprimental feature, adds generated grain based on video brightness',
-        #                    action='store_true')
-        # parser.add_argument('--grain_range')
-
-        # Vmaf
-        parser.add_argument('--vmaf', help='Calculating vmaf after encode', action='store_true')
-        parser.add_argument('--vmaf_path', type=Path, default=None, help='Path to vmaf models')
-
-        # Target Vmaf
-        parser.add_argument('--vmaf_target', type=float, help='Value of Vmaf to target')
-        parser.add_argument('--vmaf_steps', type=int, default=4, help='Steps between min and max qp for target vmaf')
-        parser.add_argument('--min_cq', type=int, default=25, help='Min cq for target vmaf')
-        parser.add_argument('--max_cq', type=int, default=50, help='Max cq for target vmaf')
-        parser.add_argument('--vmaf_plots', help='Make plots of probes in temp folder', action='store_true')
-
-        # Server parts
-        parser.add_argument('--host', nargs='+', type=str, help='ips of encoders')
-
-        # Store all vars in dictionary
-        self.d = vars(parser.parse_args())
 
     def outputs_filenames(self):
         if self.d.get('output_file'):
@@ -566,7 +501,8 @@ class Av1an:
 
     def main_thread(self):
         """Main."""
-        self.arg_parsing()
+        # Arg parse to main dictionary
+        self.d = arg_parsing()
 
         # Read/Set parameters
         self.config()
---
+++ utils/arg_parse.py
@@ -0,0 +1,63 @@
+import argparse
+from pathlib import Path
+
+def arg_parsing():
+        """Command line parsing"""
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--mode', '-m', type=int, default=0, help='0 - local, 1 - master, 2 - encoder')
+
+        # Input/Output/Temp
+        parser.add_argument('--input', '-i', nargs='+', type=Path, help='Input File')
+        parser.add_argument('--temp', type=Path, default=Path('.temp'), help='Set temp folder path')
+        parser.add_argument('--output_file', '-o', type=Path, default=None, help='Specify output file')
+
+        # Splitting
+        parser.add_argument('--split_method', type=str, default='pyscene', help='Specify splitting method')
+        parser.add_argument('--extra_split', '-xs', type=int, default=0, help='Number of frames after which make split')
+
+
+        # PySceneDetect split
+        parser.add_argument('--scenes', '-s', type=str, default=None, help='File location for scenes')
+        parser.add_argument('--threshold', '-tr', type=float, default=50, help='PySceneDetect Threshold')
+
+        # Encoding
+        parser.add_argument('--passes', '-p', type=int, default=2, help='Specify encoding passes')
+        parser.add_argument('--video_params', '-v', type=str, default='', help='encoding settings')
+        parser.add_argument('--encoder', '-enc', type=str, default='aom', help='Choosing encoder')
+        parser.add_argument('--workers', '-w', type=int, default=0, help='Number of workers')
+        parser.add_argument('-cfg', '--config', type=Path, help='Parameters file. Save/Read: '
+                                                                'Video, Audio, Encoder, FFmpeg parameteres')
+
+        # FFmpeg params
+        parser.add_argument('--ffmpeg', '-ff', type=str, default='', help='FFmpeg commands')
+        parser.add_argument('--audio_params', '-a', type=str, default='-c:a copy', help='FFmpeg audio settings')
+        parser.add_argument('--pix_format', '-fmt', type=str, default='yuv420p', help='FFmpeg pixel format')
+
+        # Misc
+        parser.add_argument('--logging', '-log', type=str, default=None, help='Enable logging')
+        parser.add_argument('--resume', '-r', help='Resuming previous session', action='store_true')
+        parser.add_argument('--no_check', '-n', help='Do not check encodings', action='store_true')
+        parser.add_argument('--keep', help='Keep temporally folder after encode', action='store_true')
+
+        # Boost
+        parser.add_argument('--boost', help='Experimental feature, decrease CQ of clip based on brightness.'
+                                            'Darker = lower CQ', action='store_true')
+        parser.add_argument('--boost_range', default=15, type=int, help='Range/strength of CQ change')
+        parser.add_argument('--boost_limit', default=10, type=int, help='CQ limit for boosting')
+
+        # Vmaf
+        parser.add_argument('--vmaf', help='Calculating vmaf after encode', action='store_true')
+        parser.add_argument('--vmaf_path', type=Path, default=None, help='Path to vmaf models')
+
+        # Target Vmaf
+        parser.add_argument('--vmaf_target', type=float, help='Value of Vmaf to target')
+        parser.add_argument('--vmaf_steps', type=int, default=4, help='Steps between min and max qp for target vmaf')
+        parser.add_argument('--min_cq', type=int, default=25, help='Min cq for target vmaf')
+        parser.add_argument('--max_cq', type=int, default=50, help='Max cq for target vmaf')
+        parser.add_argument('--vmaf_plots', help='Make plots of probes in temp folder', action='store_true')
+
+        # Server parts
+        parser.add_argument('--host', nargs='+', type=str, help='ips of encoders')
+
+        # Store all vars in dictionary
+        return vars(parser.parse_args())

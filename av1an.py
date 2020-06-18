52d64727f570897a804d77951d1ab5d824971afe
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-06-18 23:31:00 +0300

    better bar resume

--- av1an.py
+++ av1an.py
@@ -42,9 +42,15 @@ def Manager():
 
 class Counter():
     def __init__(self, total, initial):
-        self.tqdm_bar = tqdm(total=total, initial=initial, dynamic_ncols=True, unit="fr", leave=True, smoothing=0.2)
+        self.first_update = True
+        self.initial = initial
+        self.left = total - initial
+        self.tqdm_bar = tqdm(total=self.left, initial=0, dynamic_ncols=True, unit="fr", leave=True, smoothing=0.2)
 
     def update(self, value):
+        if self.first_update:
+            self.tqdm_bar.reset(self.left)
+            self.first_update = False
         self.tqdm_bar.update(value)
 
 

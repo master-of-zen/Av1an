a04458f6d72d5faa90e8e26436bb0a4c00b5aa85
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-06-22 19:31:26 +0300

    added new scripts to imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3\
 
+from .logger import log, set_log_file, set_logging
 from .arg_parse import *
 from .aom_keyframes import *
 from .boost import *
@@ -9,3 +10,5 @@ from .ffmpeg import *
 from .pyscenedetect import *
 from .utils import *
 from .vmaf import *
+from .setup import *
+from .split import *

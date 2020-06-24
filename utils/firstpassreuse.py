d77a75dac7e4f08b9ecd65d1f3f989ca0aa7649c
n9Mtq4 <bill.wresnahan@gmail.com>
2020-06-23 23:03:24 -0400

    Recompute the EOS stats for each chunk and add documentation

--- utils/firstpassreuse.py
+++ utils/firstpassreuse.py
@@ -25,21 +25,43 @@ def read_first_pass(log_path):
 
 
 def write_first_pass_log(log_path, frm_lst: List[Dict]):
+    """
+    Writes a libaom compatible first pass log from a list of dictionaries containing frame stats.
+
+    :param log_path: the path of the ouput file
+    :param frm_lst: the list of dictionaries of the frame stats + eos stat
+    :return: None
+    """
     with open(log_path, 'wb') as file:
         for frm in frm_lst:
             frm_bin = struct.pack('d' * 26, *frm.values())
             file.write(frm_bin)
 
 
-def reindex_chunk(stats: List[Dict]):
-    for i, frm_stats in enumerate(stats):
+def reindex_chunk(chunk_stats: List[Dict]):
+    """
+    The stats for each frame includes its frame number. This will reindex them to start at 0 in place.
+
+    :param chunk_stats: the list of stats for just this chunk
+    :return: None
+    """
+    for i, frm_stats in enumerate(chunk_stats):
         frm_stats['frame'] = i
 
 
-def compute_eos_stats(stats: List[Dict], old_eos: Dict):
-    # TODO(n9Mtq4): research this EOS packet and determine what actually needs to be done here. The minimal code for aom to accept it is here
+def compute_eos_stats(chunk_stats: List[Dict], old_eos: Dict):
+    """
+    The end of sequence stat is a final packet at the end of the log. It contains the sum of all the previous
+    frame packets. When we split the log file, we need to sum up just the included frames as a new EOS packet.
+
+    :param chunk_stats: the list of stats for just this chunk
+    :param old_eos: the old eos stat packet
+    :return: A dict for the new eos packet
+    """
     eos = old_eos.copy()
-    eos['count'] = len(stats)
+    for key in eos.keys():
+        eos[key] = sum([d[key] for d in chunk_stats])
+        # eos[key] = (old_eos[key] / old_eos['count']) * len(chunk_stats)  # TODO(n9Mtq4): I think this will work well for VBR encodes
     return eos
 
 
@@ -48,10 +70,12 @@ def segment_first_pass(temp, framenums):
     stats = read_first_pass(stat_file)
 
     # special case for only 1 scene
+    # we don't need to do anything with the log
     if len(framenums) == 0:
         write_first_pass_log(os.path.join(temp, "split", "0.log"), stats)
         return
 
+    eos_stats = stats[-1]  # EOS stats is the last one
     split_names = [str(i).zfill(5) for i in range(len(framenums) + 1)]
     frm_split = [0] + framenums + [len(stats) - 1]
 
@@ -62,6 +86,6 @@ def segment_first_pass(temp, framenums):
 
         chunk_stats = stats[frm_start_idx:frm_end_idx]
         reindex_chunk(chunk_stats)
-        chunk_stats = chunk_stats + [compute_eos_stats(chunk_stats, stats[-1])]
+        chunk_stats = chunk_stats + [compute_eos_stats(chunk_stats, eos_stats)]
 
         write_first_pass_log(os.path.join(temp, "split", log_name), chunk_stats)

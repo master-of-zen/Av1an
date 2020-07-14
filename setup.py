fb24b83e1a56c37d9ebbbba0c54d8b838484fe66
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-07-14 04:47:22 +0300

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="Av1an",
-    version="1.10.7",
+    version="1.10.8",
     author="Master_Of_Zen",
     author_email="master_of_zen@protonmail.com",
     description="All-in-one encode toolkit",

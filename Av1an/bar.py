980c966e532675d115f2a049d222f8f23ee54420
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-07-25 15:52:06 +0300

    better smoothing for bar

--- Av1an/bar.py
+++ Av1an/bar.py
@@ -22,7 +22,7 @@ class Counter():
         self.first_update = True
         self.initial = initial
         self.left = total - initial
-        self.tqdm_bar = tqdm(total=self.left, initial=0, dynamic_ncols=True, unit="fr", leave=True, smoothing=0.2)
+        self.tqdm_bar = tqdm(total=self.left, initial=0, dynamic_ncols=True, unit="fr", leave=True, smoothing=0.01)
 
     def update(self, value):
         if self.first_update:

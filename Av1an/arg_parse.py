50857955bcf5cd6ec5cd0657630540bce2b9ecaf
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-08-05 20:55:13 +0300

    probes to 4, rate to 4

--- Av1an/arg_parse.py
+++ Av1an/arg_parse.py
@@ -113,11 +113,11 @@ def arg_parsing():
 
     # Target Vmaf
     parser.add_argument('--vmaf_target', type=float, help='Value of Vmaf to target')
-    parser.add_argument('--vmaf_steps', type=int, default=5, help='Steps between min and max qp for target vmaf')
+    parser.add_argument('--vmaf_steps', type=int, default=4, help='Steps between min and max qp for target vmaf')
     parser.add_argument('--min_q', type=int, default=None, help='Min q for target vmaf')
     parser.add_argument('--max_q', type=int, default=None, help='Max q for target vmaf')
     parser.add_argument('--vmaf_plots', help='Make plots of probes in temp folder', action='store_true')
-    parser.add_argument('--vmaf_rate', type=int, default=6, help='Framerate for probes, 0 - original')
+    parser.add_argument('--vmaf_rate', type=int, default=4, help='Framerate for probes, 0 - original')
     parser.add_argument('--n_threads', type=int, default=None, help='Threads for vmaf calculation')
 
     # VVC

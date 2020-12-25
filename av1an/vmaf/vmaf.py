947128fff683df27fad91dca435235c10410fce1
Zen <true.grenight@gmail.com>
2020-12-26 00:41:21 +0200

    Added vmaf validation

--- av1an/vmaf/vmaf.py
+++ av1an/vmaf/vmaf.py
@@ -4,6 +4,7 @@ import json
 import shlex
 import subprocess
 import sys
+from collections import deque
 
 from pathlib import Path
 from subprocess import PIPE, STDOUT
@@ -28,6 +29,37 @@ class VMAF:
         self.model = f":model_path={model}" if model else ''
         self.res = res if res else "1920x1080"
         self.vmaf_filter = vmaf_filter + ',' if vmaf_filter else ''
+        self.validate_vmaf()
+
+    def validate_vmaf(self):
+        """
+        Test run of ffmpeg for validating that ffmpeg/libmaf/models properly setup
+        """
+
+        if self.model or self.n_threads:
+            add = f'={self.model}{self.n_threads}'
+        else:
+            add = ''
+
+        cmd = f' ffmpeg -hide_banner -filter_complex testsrc=duration=1:size=28x16:rate=1[B];testsrc=duration=1:size=28x16:rate=1[A];[B][A]libvmaf{add} -t 1  -f null - '.split()
+
+        pipe = subprocess.Popen(cmd, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
+
+        encoder_history = deque(maxlen=30)
+
+        while True:
+            line = pipe.stdout.readline().strip()
+            if len(line) == 0 and pipe.poll() is not None:
+                break
+            if len(line) == 0:
+                continue
+            if line:
+                encoder_history.append(line)
+
+        if pipe.returncode != 0 and pipe.returncode != -2:
+            print(f"\n:: VMAF validation error: {pipe.returncode}")
+            print('\n'.join(encoder_history))
+            sys.exit()
 
     @staticmethod
     def read_json(file):
@@ -155,7 +187,7 @@ class VMAF:
         """
         Running vmaf on 2 files and returning file
         """
-        
+
         if not all((type(source) == Path, type(encoded) == Path)):
             source = Path(source)
             encoded = Path(encoded)

1ba2515f813832ce32cb2d54d1835f1bfa1f1cfe
Zen <true.grenight@gmail.com>
2020-12-27 06:58:18 +0200

    +

--- av1an/manager/Manager.py
+++ av1an/manager/Manager.py
@@ -26,8 +26,9 @@ from av1an.target_quality import (per_frame_target_quality_routine,
                                   per_shot_target_quality_routine)
 from av1an.utils import frame_probe, frame_probe_fast, terminate
 from av1an.vmaf import VMAF
-from tqdm import tqdm
-from .Counter import Manager, BaseManager, Counter
+
+from .Counter import BaseManager, Counter, Manager
+from .Pipes import tqdm_bar
 
 
 class Main:
@@ -213,69 +214,3 @@ class EncodingManager:
             print(f':: Chunk #{chunk.name}: {actual_frames}/{expected_frames} fr')
         return actual_frames
 
-
-def process_pipe(pipe, chunk: Chunk):
-    encoder_history = deque(maxlen=20)
-    while True:
-        line = pipe.stdout.readline().strip()
-        if len(line) == 0 and pipe.poll() is not None:
-            break
-        if len(line) == 0:
-            continue
-        if line:
-            encoder_history.append(line)
-
-    if pipe.returncode != 0 and pipe.returncode != -2:
-        print(f"\n:: Encoder encountered an error: {pipe.returncode}")
-        print(f"\n:: Chunk: {chunk.index}")
-        print('\n'.join(encoder_history))
-
-
-def process_encoding_pipe(pipe, encoder, counter, chunk: Chunk):
-    encoder_history = deque(maxlen=20)
-    frame = 0
-    enc = ENCODERS[encoder]
-    while True:
-        line = pipe.stdout.readline().strip()
-
-        if len(line) == 0 and pipe.poll() is not None:
-            break
-
-        if len(line) == 0:
-            continue
-
-        match = enc.match_line(line)
-
-        if match:
-            new = int(match.group(1))
-            if new > frame:
-                counter.update(new - frame)
-                frame = new
-
-        if line:
-            encoder_history.append(line)
-
-    if pipe.returncode != 0 and pipe.returncode != -2:  # -2 is Ctrl+C for aom
-
-        print(f"\n:: Encoder encountered an error: {pipe.returncode}")
-        print(f"\n:: Chunk: {chunk.index}")
-        print('\n'.join(encoder_history))
-
-
-def tqdm_bar(a: Project, c: Chunk, encoder, counter, frame_probe_source, passes, current_pass):
-    try:
-
-        enc = ENCODERS[encoder]
-        pipe = enc.make_pipes(a, c, passes, current_pass, c.output)
-
-        if encoder in ('aom', 'vpx', 'rav1e', 'x265', 'x264', 'vvc', 'svt_av1'):
-            process_encoding_pipe(pipe, encoder, counter, c)
-
-        if encoder in ('svt_vp9'):
-            # SVT-VP9 is special
-            process_pipe(pipe, c)
-            counter.update(frame_probe_source // passes)
-
-    except Exception as e:
-        _, _, exc_tb = sys.exc_info()
-        print(f'\n:: Error at encode {e}\nAt line {exc_tb.tb_lineno}')

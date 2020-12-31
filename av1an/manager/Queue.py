7b3f4fb983248f47f678672247c31abd0f661f52
Zen <true.grenight@gmail.com>
2020-12-31 22:22:29 +0200

    wip

--- av1an/manager/Queue.py
+++ av1an/manager/Queue.py
@@ -1,1 +1,10 @@
- 
+
+
+from collections import deque
+
+class Queue:
+    """
+    Queue manager with ability to add/remove/restart jobs
+    """
+    def __init__(self, workers):
+        self.queue = deque(maxlen=workers)

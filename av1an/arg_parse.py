80a0e50ff78aad0b9deab5a33e3651f0ef6264d7
Zen <true.grenight@gmail.com>
2021-02-10 15:38:28 +0200

    version bump to 6

--- av1an/arg_parse.py
+++ av1an/arg_parse.py
@@ -284,6 +284,6 @@ class Args:
         misc_group = parser.add_argument_group('Misc')
         misc_group.add_argument('--version',
                                 action='version',
-                                version=f'Av1an version: 5.7')
+                                version=f'Av1an version: 6')
         # Initialize project with initial values
         return parser
--- setup-minimal.py
+++ setup-minimal.py
@@ -9,7 +9,7 @@ REQUIRES = [
 with open("README.md", "r") as f:
     long_description = f.read()
 
-version = "5.7"
+version = "6"
 
 setuptools.setup(
     name="Av1an-minimal",

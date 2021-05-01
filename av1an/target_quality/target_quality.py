20eeade7409f98f8b726cb1698fbbbef4ec0222f
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-05-02 00:18:39 +0300

    svt_av1 target quality fix

--- av1an/target_quality/target_quality.py
+++ av1an/target_quality/target_quality.py
@@ -529,7 +529,7 @@ class TargetQuality:
                 "--tf-level",
                 "3",
             ]
-            cmd = CommandPair(pipe, [*params, "-b", probe_name, "-"])
+            cmd = CommandPair(pipe, [*params, "-b", probe_name])
 
         elif encoder == "svt_vp9":
             params = [

0beaadcdf726e26e802df430baec8a5401f2723b
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-05-14 21:19:16 +0300

    path hashing to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use std::process::{Command, Stdio};
+use std::{
+  collections::hash_map::DefaultHasher,
+  hash::{Hash, Hasher},
+};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -14,10 +18,20 @@ fn get_ffmpeg_info() -> PyResult<String> {
   Ok(output)
 }
 
+#[pyfunction]
+fn hash_path(path: String) -> PyResult<String> {
+  let mut s = DefaultHasher::new();
+  path.hash(&mut s);
+  let hs = s.finish().to_string();
+  let out = hs[0..7].to_string();
+  Ok(out)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn av1an(_py: Python, m: &PyModule) -> PyResult<()> {
   m.add_function(wrap_pyfunction!(get_ffmpeg_info, m)?)?;
+  m.add_function(wrap_pyfunction!(hash_path, m)?)?;
 
   Ok(())
 }

78b3395ddaf153d038f7a85290600001c9d8ae33
Josh Holmer <jholmer.in@gmail.com>
2021-12-29 15:30:10 -0500

    Fix more clippy lints (#476)

--- av1an-core/src/parse.rs
+++ av1an-core/src/parse.rs
@@ -337,6 +337,7 @@ mod tests {
   use crate::parse::*;
 
   #[test]
+  #[allow(clippy::cognitive_complexity)]
   fn valid_params_works() {
     use std::borrow::Borrow;
 
@@ -408,8 +409,8 @@ mod tests {
       ("240 frames: 39.11 fps, 14.60 kb/s", Some(240)),
       ("2445 frames: 39.11 fps, 14.60 kb/s", Some(2445)),
       ("24145 frames: 39.11 fps, 14.60 kb/s", Some(24145)),
-      ("246434 frames: 39.11 fps, 14.60 kb/s", Some(246434)),
-      ("2448732 frames: 39.11 fps, 14.60 kb/s", Some(2448732)),
+      ("246434 frames: 39.11 fps, 14.60 kb/s", Some(246_434)),
+      ("2448732 frames: 39.11 fps, 14.60 kb/s", Some(2_448_732)),
       ("invalid data", None),
       ("", None),
     ];
@@ -427,8 +428,11 @@ mod tests {
       ("Encoding frame   22 2.03 kbps 3.68 fps", Some(22)),
       ("Encoding frame  7654 1.08 kbps 2.00 fps", Some(7654)),
       ("Encoding frame 72415 1.08 kbps 2.00 fps", Some(72415)),
-      ("Encoding frame 778743 1.08 kbps 2.00 fps", Some(778743)),
-      ("Encoding frame 53298734 1.08 kbps 2.00 fps", Some(53298734)),
+      ("Encoding frame 778743 1.08 kbps 2.00 fps", Some(778_743)),
+      (
+        "Encoding frame 53298734 1.08 kbps 2.00 fps",
+        Some(53_298_734),
+      ),
       ("invalid input", None),
       ("", None),
     ];
@@ -473,11 +477,11 @@ mod tests {
       ("invalid data", None),
       (
         "Pass 2/2 frame 102262/102261 136473850B  131502 ms 777.65 fps [ETA  unknown]    1272F",
-        Some(102261),
+        Some(102_261),
       ),
       (
         "Pass 1/1 frame 1022621/1022611 136473850B  131502 ms 777.65 fps [ETA  unknown]    1272F",
-        Some(1022611),
+        Some(1_022_611),
       ),
     ];
 
@@ -490,7 +494,7 @@ mod tests {
     }
 
     for (s, ans) in test_cases {
-      assert_eq!(parse_aom_vpx_frames(&s), ans);
+      assert_eq!(parse_aom_vpx_frames(s), ans);
     }
   }
 }

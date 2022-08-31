760988da30e98082f9fbe409a7c2e3c7f6be2a9e
Redzic <yusufredzic@gmail.com>
2022-08-30 19:37:43 -0500

    temporary

--- compare.sh
+++ compare.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# echo $1
-
 rm master.json zerocopy.json
 
-./target/release/av1an -i $1 --sc-method fast --sc-only -s zerocopy.json
-/usr/bin/av1an -i $1 --sc-method fast --sc-only -s master.json
+./target/release/av1an -i "$1" --sc-method fast --sc-only -s zerocopy.json
+/usr/bin/av1an -i "$1" --sc-method fast --sc-only -s master.json
 
 cmp master.json zerocopy.json

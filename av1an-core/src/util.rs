c4a0bcaad898715a515d286ce0885e29a8a23f86
Josh Holmer <jholmer.in@gmail.com>
2024-07-23 19:21:02 -0400

    Use absolute instead of canonicalize for better Windows path handling
    Closes #860

--- av1an-core/src/util.rs
+++ av1an-core/src/util.rs
@@ -1,4 +1,7 @@
-use std::path::{Path, PathBuf};
+use std::{
+  io,
+  path::{absolute, Path, PathBuf},
+};
 
 /// Count the number of elements passed to this macro.
 ///
@@ -131,6 +134,15 @@ pub fn read_in_dir(path: &Path) -> anyhow::Result<impl Iterator<Item = PathBuf>>
       }
     })
   }))
+}
+
+#[inline]
+pub(crate) fn to_absolute_path(path: &Path) -> io::Result<PathBuf> {
+  if cfg!(target_os = "windows") {
+    absolute(path)
+  } else {
+    path.canonicalize()
+  }
 }
 
 #[cfg(test)]

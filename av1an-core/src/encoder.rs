5e56c2f384380b78387e042e9c250e9d726aa41b
Josh Holmer <jholmer.in@gmail.com>
2024-12-01 15:31:46 -0500

    Fix compatibility with x265 4.0 (#893)

--- av1an-core/src/encoder.rs
+++ av1an-core/src/encoder.rs
@@ -149,7 +149,7 @@ impl Encoder {
       Self::x265 => chain!(
         into_array!["x265", "--y4m", "--frames", frame_count.to_string()],
         params,
-        into_array!["-", "-o", output]
+        into_array!["--input", "-", "-o", output]
       )
       .collect(),
     }
@@ -232,6 +232,7 @@ impl Encoder {
           format!("{fpf}.log"),
           "--analysis-reuse-file",
           format!("{fpf}_analysis.dat"),
+          "--input",
           "-",
           "-o",
           NULL
@@ -331,6 +332,7 @@ impl Encoder {
           format!("{fpf}.log"),
           "--analysis-reuse-file",
           format!("{fpf}_analysis.dat"),
+          "--input",
           "-",
           "-o",
           output

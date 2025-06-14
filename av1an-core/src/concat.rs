master-of-zen/Av1an

commit fbba019ea5ba6ed6d2ae56b2645e118c9aeed8af

Set the output FPS when concatenating
This ensures that certain encoders such as vpx which do not properly set
the frame rate on their outputs do not cause av1an to output videos with
the wrong frame rate. This only works with mkvmerge concat method.
As a result of this plus the existing issue with ffmpeg not
concatenating x265 outputs correctly, this also changes the default
concat method to mkvmerge.

Fixes #475

av1an-core/src/concat.rs
@@ -9,6 +9,7 @@ use anyhow::{anyhow, Context};
 use av_format::buffer::AccReader;
 use av_format::demuxer::{Context as DemuxerContext, Event};
 use av_format::muxer::{Context as MuxerContext, Writer};
+use av_format::rational::Rational64;
 use av_ivf::demuxer::IvfDemuxer;
 use av_ivf::muxer::IvfMuxer;
 use path_abs::{PathAbs, PathInfo};
@@ -154,6 +155,7 @@ pub fn mkvmerge(
   output: &Path,
   encoder: Encoder,
   num_chunks: usize,
+  output_fps: Rational64,
 ) -> anyhow::Result<()> {
   // mkvmerge does not accept UNC paths on Windows
   #[cfg(windows)]
@@ -199,6 +201,7 @@ pub fn mkvmerge(
     encoder,
     &fix_path(output.to_str().unwrap()),
     audio_file.as_deref(),
+    output_fps,
   );
 
   let mut options_json = File::create(options_path)?;
@@ -232,13 +235,20 @@ pub fn mkvmerge_options_json(
   encoder: Encoder,
   output: &str,
   audio: Option<&str>,
+  output_fps: Rational64,
 ) -> String {
   let mut file_string = String::with_capacity(64 + 12 * num);
   write!(file_string, "[\"-o\", {output:?}").unwrap();
   if let Some(audio) = audio {
     write!(file_string, ", {audio:?}").unwrap();
   }
-  file_string.push_str(", \"[\"");
+  write!(
+    file_string,
+    ", \"--default-duration\", \"0:{}/{}fps\", \"[\"",
+    output_fps.numer(),
+    output_fps.denom()
+  )
+  .unwrap();
   for i in 0..num {
     write!(file_string, ", \"{i:05}.{}\"", encoder.output_extension()).unwrap();
   }
@@ -247,7 +257,7 @@ pub fn mkvmerge_options_json(
   file_string
 }
 
-/// Concatenates using ffmpeg (does not work with x265)
+/// Concatenates using ffmpeg (does not work with x265, and may have incorrect FPS with vpx)
 #[tracing::instrument(level = "debug")]
 pub fn ffmpeg(temp: &Path, output: &Path) -> anyhow::Result<()> {
   fn write_concat_file(temp_folder: &Path) -> anyhow::Result<()> {
@@ -350,4 +360,34 @@ pub fn ffmpeg(temp: &Path, output: &Path) -> anyhow::Result<()> {
   }
 
   Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use av_format::rational::Rational64;
+
+  #[test]
+  fn test_mkvmerge_options_json_no_audio() {
+    let result = mkvmerge_options_json(2, Encoder::aom, "output.mkv", None, Rational64::new(30, 1));
+    assert_eq!(
+      result,
+      r#"["-o", "output.mkv", "--default-duration", "0:30/1fps", "[", "00000.ivf", "00001.ivf","]"]"#
+    );
+  }
+
+  #[test]
+  fn test_mkvmerge_options_json_with_audio() {
+    let result = mkvmerge_options_json(
+      2,
+      Encoder::aom,
+      "output.mkv",
+      Some("audio.mkv"),
+      Rational64::new(30, 1),
+    );
+    assert_eq!(
+      result,
+      r#"["-o", "output.mkv", "audio.mkv", "--default-duration", "0:30/1fps", "[", "00000.ivf", "00001.ivf","]"]"#
+    );
+  }
 }

av1an-core/src/context.rs
@@ -17,6 +17,7 @@ use anyhow::{bail, Context};
 use av1_grain::TransferFunction;
 use crossbeam_utils;
 use itertools::Itertools;
+use num_traits::cast::ToPrimitive;
 use rand::prelude::SliceRandom;
 use rand::rng;
 use tokio::io::{AsyncBufReadExt, BufReader};
@@ -190,7 +191,8 @@ impl Av1anContext {
         };
 
     let res = self.args.input.resolution()?;
-    let fps = self.args.input.frame_rate()?;
+    let fps_ratio = self.args.input.frame_rate()?;
+    let fps = fps_ratio.to_f64().unwrap();
     let format = self.args.input.pixel_format()?;
     let tfc = self
       .args
@@ -309,9 +311,8 @@ impl Av1anContext {
       }
 
       if chunks_done > 0 {
-        let frame_rate = self.args.input.frame_rate()?;
         update_progress_bar_estimates(
-          frame_rate,
+          fps,
           self.frames,
           self.args.verbosity,
           (chunks_done as u32, total_chunks as u32),
@@ -357,6 +358,7 @@ impl Av1anContext {
             self.args.output_file.as_ref(),
             self.args.encoder,
             total_chunks,
+            fps_ratio,
           )?;
         }
         ConcatMethod::FFmpeg => {
@@ -1011,7 +1013,7 @@ impl Av1anContext {
   }
 
   fn create_video_queue_vs(&self, scenes: &[Scene], vs_script: &Path) -> Vec<Chunk> {
-    let frame_rate = self.args.input.frame_rate().unwrap();
+    let frame_rate = self.args.input.frame_rate().unwrap().to_f64().unwrap();
     let chunk_queue: Vec<Chunk> = scenes
       .iter()
       .enumerate()
@@ -1027,7 +1029,7 @@ impl Av1anContext {
 
   fn create_video_queue_select(&self, scenes: &[Scene]) -> Vec<Chunk> {
     let input = self.args.input.as_video_path();
-    let frame_rate = self.args.input.frame_rate().unwrap();
+    let frame_rate = self.args.input.frame_rate().unwrap().to_f64().unwrap();
 
     let chunk_queue: Vec<Chunk> = scenes
       .iter()
@@ -1051,7 +1053,7 @@ impl Av1anContext {
 
   fn create_video_queue_segment(&self, scenes: &[Scene]) -> anyhow::Result<Vec<Chunk>> {
     let input = self.args.input.as_video_path();
-    let frame_rate = self.args.input.frame_rate().unwrap();
+    let frame_rate = self.args.input.frame_rate().unwrap().to_f64().unwrap();
 
     debug!("Splitting video");
     segment(
@@ -1093,7 +1095,7 @@ impl Av1anContext {
 
   fn create_video_queue_hybrid(&self, scenes: &[Scene]) -> anyhow::Result<Vec<Chunk>> {
     let input = self.args.input.as_video_path();
-    let frame_rate = self.args.input.frame_rate().unwrap();
+    let frame_rate = self.args.input.frame_rate().unwrap().to_f64().unwrap();
 
     let keyframes = crate::ffmpeg::get_keyframes(input).unwrap();
 

.github/workflows/tests.yml
@@ -270,14 +270,14 @@ jobs:
           target/ci/av1an -i tt_sif.y4m -e aom --pix-format yuv420p --sc-method fast -y -o "tt_sif.mkv" -v " --cpu-used=10 --rt --threads=4" --temp temporary
           du -h tt_sif.mkv
 
-      - name: Testing concat mkvmerge aom
+      - name: Testing concat ffmpeg aom
         run: |
-          target/ci/av1an -i tt_sif.y4m -e aom --pix-format yuv420p --sc-method fast -y -o "tt_sif.mkv" -v " --cpu-used=10 --rt --threads=4" -c mkvmerge
+          target/ci/av1an -i tt_sif.y4m -e aom --pix-format yuv420p --sc-method fast -y -o "tt_sif.mkv" -v " --cpu-used=10 --rt --threads=4" -c ffmpeg
           du -h tt_sif.mkv
 
       - name: Testing slow scenechange aom
         run: |
-          target/ci/av1an -i tt_sif.y4m -e aom --pix-format yuv420p -y -o "tt_sif.mkv" -v " --cpu-used=10 --rt --threads=4" -c mkvmerge
+          target/ci/av1an -i tt_sif.y4m -e aom --pix-format yuv420p -y -o "tt_sif.mkv" -v " --cpu-used=10 --rt --threads=4"
           du -h tt_sif.mkv
 
       - name: Testing sc-only

av1an-core/Cargo.toml
@@ -59,6 +59,7 @@ tokio = { version = "1.43", features = ["full"] }
 tracing-subscriber = { version = "0.3", features = ["env-filter"] }
 regex = "1.11.1"
 ctrlc = "3.4.6"
+num-traits = "0.2.19"
 
 # TODO: https://github.com/elast0ny/affinity/issues/2
 # update this when macos support is implemented

av1an-core/src/ffmpeg.rs
@@ -1,12 +1,12 @@
-use std::ffi::OsStr;
-use std::path::{Path, PathBuf};
-use std::process::{Command, Stdio};
-
+use av_format::rational::Rational64;
 use ffmpeg::color::TransferCharacteristic;
 use ffmpeg::format::{input, Pixel};
 use ffmpeg::media::Type as MediaType;
 use ffmpeg::Error::StreamNotFound;
 use path_abs::{PathAbs, PathInfo};
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
 
 use crate::{into_array, into_vec};
 
@@ -59,14 +59,17 @@ pub fn num_frames(source: &Path) -> Result<usize, ffmpeg::Error> {
 }
 
 #[tracing::instrument(level = "debug")]
-pub fn frame_rate(source: &Path) -> Result<f64, ffmpeg::Error> {
+pub fn frame_rate(source: &Path) -> Result<Rational64, ffmpeg::Error> {
   let ictx = input(source)?;
   let input = ictx
     .streams()
     .best(MediaType::Video)
     .ok_or(StreamNotFound)?;
   let rate = input.avg_frame_rate();
-  Ok(f64::from(rate.numerator()) / f64::from(rate.denominator()))
+  Ok(Rational64::new(
+    rate.numerator() as i64,
+    rate.denominator() as i64,
+  ))
 }
 
 #[tracing::instrument(level = "debug")]

av1an-core/src/lib.rs
@@ -19,6 +19,7 @@ use ::vapoursynth::api::API;
 use ::vapoursynth::map::OwnedMap;
 use anyhow::{bail, Context};
 use av1_grain::TransferFunction;
+use av_format::rational::Rational64;
 use chunk::Chunk;
 use dashmap::DashMap;
 use once_cell::sync::OnceCell;
@@ -113,7 +114,7 @@ impl Input {
     })
   }
 
-  pub fn frame_rate(&self) -> anyhow::Result<f64> {
+  pub fn frame_rate(&self) -> anyhow::Result<Rational64> {
     const FAIL_MSG: &str = "Failed to get frame rate for input video";
     Ok(match &self {
       Input::Video { path } => {

av1an-core/src/settings.rs
@@ -126,6 +126,10 @@ impl EncodeArgs {
 properly into a mkv file. Specify mkvmerge as the concatenation method by setting `--concat mkvmerge`.");
     }
 
+    if self.encoder == Encoder::vpx && self.concat != ConcatMethod::MKVMerge {
+      warn!("mkvmerge is recommended for concatenating vpx, as vpx outputs with incorrect frame rates, which we can only resolve using mkvmerge. Specify mkvmerge as the concatenation method by setting `--concat mkvmerge`.");
+    }
+
     if self.chunk_method == ChunkMethod::LSMASH {
       ensure!(
         is_lsmash_installed(),

av1an-core/src/vapoursynth.rs
@@ -5,6 +5,7 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use anyhow::{anyhow, bail};
+use av_format::rational::Rational64;
 use once_cell::sync::Lazy;
 use path_abs::PathAbs;
 use regex::Regex;
@@ -125,12 +126,15 @@ fn get_num_frames(env: &Environment) -> anyhow::Result<usize> {
   Ok(num_frames)
 }
 
-fn get_frame_rate(env: &Environment) -> anyhow::Result<f64> {
+fn get_frame_rate(env: &Environment) -> anyhow::Result<Rational64> {
   let info = get_clip_info(env);
 
   match info.framerate {
     Property::Variable => bail!("Cannot output clips with varying framerate"),
-    Property::Constant(fps) => Ok(fps.numerator as f64 / fps.denominator as f64),
+    Property::Constant(fps) => Ok(Rational64::new(
+      fps.numerator as i64,
+      fps.denominator as i64,
+    )),
   }
 }
 
@@ -346,7 +350,7 @@ pub fn bit_depth(source: &Path, vspipe_args_map: OwnedMap) -> anyhow::Result<usi
   get_bit_depth(&environment)
 }
 
-pub fn frame_rate(source: &Path, vspipe_args_map: OwnedMap) -> anyhow::Result<f64> {
+pub fn frame_rate(source: &Path, vspipe_args_map: OwnedMap) -> anyhow::Result<Rational64> {
   // Create a new VSScript environment.
   let mut environment = Environment::new().unwrap();
 

commit 39a899bd28c28b11a38e332d5b79393e3637571d

fix fbba019e: forcing output FPS breaks using FPS changing filter (#1017)
Hi,

I use FFmpeg on an input filter to change fps of some of my clips:
`--ignore-frame-mismatch --ffmpeg "-vf
scale=-1:\\'min(ih,1080)\\':flags=lanczos,fps=15"`. After updating to
newest master I found that fbba019e broke my method: since now, the
output muxer is forced unconditionally to emit videos @ input fps, I get
a video where e.g. with fps 60->15 The first 25% of the file play the
whole video in 4x speed, and the last 75% are still image.

This PR would add a condition to only force output fps if
`--ignore-frame-mismatch` is not set, since if you want to change output
fps with FFmpeg you have to use this flag. I would also be down to add a
flag to av1an to control fps forcing, or --- maybe even better --- add a
flag to change fps directly through Av1an. This would additionally
provide accurate progress bar data, which is broken with this method.

@Maintainers: please tell me which solution you would accept upstream, I
will do my best to implement them :)

Co-authored-by: t-nil <t-nil@t-nil.dev>

av1an-core/src/concat.rs
@@ -162,7 +162,7 @@ pub fn mkvmerge(
     output: &Path,
     encoder: Encoder,
     num_chunks: usize,
-    output_fps: Rational64,
+    output_fps: Option<Rational64>,
 ) -> anyhow::Result<()> {
     // mkvmerge does not accept UNC paths on Windows
     #[cfg(windows)]
@@ -242,20 +242,24 @@ pub fn mkvmerge_options_json(
     encoder: Encoder,
     output: &str,
     audio: Option<&str>,
-    output_fps: Rational64,
+    output_fps: Option<Rational64>,
 ) -> String {
     let mut file_string = String::with_capacity(64 + 12 * num);
     write!(file_string, "[\"-o\", {output:?}").unwrap();
     if let Some(audio) = audio {
         write!(file_string, ", {audio:?}").unwrap();
     }
-    write!(
-        file_string,
-        ", \"--default-duration\", \"0:{}/{}fps\", \"[\"",
-        output_fps.numer(),
-        output_fps.denom()
-    )
-    .unwrap();
+    if let Some(output_fps) = output_fps {
+        write!(
+            file_string,
+            ", \"--default-duration\", \"0:{}/{}fps\", \"[\"",
+            output_fps.numer(),
+            output_fps.denom()
+        )
+        .unwrap();
+    } else {
+        file_string.push_str(", \"[\"");
+    }
     for i in 0..num {
         write!(
             file_string,

av1an-core/src/concat/tests.rs
@@ -4,7 +4,13 @@ use super::*;
 
 #[test]
 fn test_mkvmerge_options_json_no_audio() {
-    let result = mkvmerge_options_json(2, Encoder::aom, "output.mkv", None, Rational64::new(30, 1));
+    let result = mkvmerge_options_json(
+        2,
+        Encoder::aom,
+        "output.mkv",
+        None,
+        Some(Rational64::new(30, 1)),
+    );
     assert_eq!(
         result,
         r#"["-o", "output.mkv", "--default-duration", "0:30/1fps", "[", "00000.ivf", "00001.ivf","]"]"#
@@ -18,7 +24,7 @@ fn test_mkvmerge_options_json_with_audio() {
         Encoder::aom,
         "output.mkv",
         Some("audio.mkv"),
-        Rational64::new(30, 1),
+        Some(Rational64::new(30, 1)),
     );
     assert_eq!(
         result,

av1an-core/src/context.rs
@@ -391,7 +391,19 @@ impl Av1anContext {
                         self.args.output_file.as_ref(),
                         self.args.encoder,
                         total_chunks,
-                        fps_ratio,
+                        if self.args.ignore_frame_mismatch {
+                            info!(
+                                "`--ignore-frame-mismatch` set. Don't force output FPS, as an FPS \
+                                 changing filter might have been applied."
+                            );
+                            None
+                        } else {
+                            debug!(
+                                "`--ignore-frame-mismatch` not set. Forcing output FPS to \
+                                 {fps_ratio} with mkvmerge."
+                            );
+                            Some(fps_ratio)
+                        },
                     )?;
                 },
                 ConcatMethod::FFmpeg => {